ed4888f900609c9e40679088b83b48e80f77b2c5
Daniel Hao Huynh <huynhdaniel10@gmail.com>
2022-04-05 00:34:55 +0200

    Added Firebase support, and hidden the service key within gitignore. closes #2

--- functions/serverFuncs.go
+++ functions/serverFuncs.go
@@ -2,13 +2,27 @@ package funcs
 
 import (
 	consts "assignment-2/constants"
+	"context"
 	"log"
 	"net/http"
 	"os"
+
+	firestore "cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
 func IsEmpty(inn string) bool {
 	return !(len(inn) > 0)
+}
+
+/**	checkError logs an error.
+ *	@param inn - error value
+ */
+func checkError(inn error) {
+	if inn != nil {
+		log.Fatal(inn)
+	}
 }
 
 func SetPort(inn string) string {
@@ -23,4 +37,26 @@ func SetPort(inn string) string {
 func SetListener(inn string) {
 	log.Println("Listening on port " + inn)
 	log.Fatal(http.ListenAndServe(":"+inn, nil))
+}
+
+func SetServiceAcc(ctx context.Context, serviceKey string) *firebase.App {
+	servAcc := option.WithCredentialsFile(serviceKey)
+	app, err := firebase.NewApp(ctx, nil, servAcc)
+	if err != nil {
+		log.Fatal("error initializing app:", err)
+	}
+	return app
+}
+
+func InstantiateFBClient(app *firebase.App, ctx context.Context) *firestore.Client {
+	client, err := app.Firestore(ctx)
+	checkError(err)
+	return client
+}
+
+func CloseFB(client *firestore.Client) {
+	err := client.Close()
+	if err != nil {
+		log.Fatal("Closing of the firebase client failed. Error:", err)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -3,16 +3,38 @@ package main
 import (
 	consts "assignment-2/constants"
 	funcs "assignment-2/functions"
+	"context"
+
+	"cloud.google.com/go/firestore"
+	// Firebase Dependancies
 )
+
+// Firebase context and client used by Firestore functions throughout the program.
+var ctx context.Context
+var client *firestore.Client
+
+// Collection name in Firestore
+const collection = "messages"
 
 /**
  *	The main function.
  *	Creates a link/routes to the correct handlers
  */
-
 func main() {
 	funcs.TimerStart(funcs.Timer)
 	port := funcs.SetPort(consts.DEFAULT_PORT)
+	// Firebase initialisation
+	ctx = context.Background()
 
+	// We use a service account, load credentials file that you downloaded from your project's settings menu.
+	app := funcs.SetServiceAcc(ctx, "serviceKey/serviceAccountKey.json")
+
+	// Instantiate client
+	client := funcs.InstantiateFBClient(app, ctx)
+
+	// Close down client
+	defer funcs.CloseFB(client)
+
+	// listen for port
 	funcs.SetListener(port)
 }
